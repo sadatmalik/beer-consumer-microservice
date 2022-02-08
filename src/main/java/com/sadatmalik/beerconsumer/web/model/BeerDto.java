223de53ecd2b0c92c5532cf91d5576123c283106
sadat.malik <sadat.malik@me.com>
2022-02-08 14:41:27 +0000

    Jackson annotations

--- src/main/java/com/sadatmalik/beerconsumer/web/model/BeerDto.java
+++ src/main/java/com/sadatmalik/beerconsumer/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.sadatmalik.beerconsumer.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,9 +26,11 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -42,6 +45,7 @@ public class BeerDto {
 
     @Positive
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
     private Integer quantityOnHand;
