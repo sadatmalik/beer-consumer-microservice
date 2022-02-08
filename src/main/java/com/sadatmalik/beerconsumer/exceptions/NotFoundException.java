638263b3e701a680b6f60493d4d918f358c1a164
sadat.malik <sadat.malik@me.com>
2022-02-08 15:00:45 +0000

    Changing UPC to String, implementing BeerService, updating bootstrapping and tests

---
+++ src/main/java/com/sadatmalik/beerconsumer/exceptions/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.sadatmalik.beerconsumer.exceptions;
+
+public class NotFoundException extends RuntimeException {
+}
---
+++ src/main/java/com/sadatmalik/beerconsumer/services/BeerServiceImpl.java
@@ -0,0 +1,42 @@
+package com.sadatmalik.beerconsumer.services;
+
+import com.sadatmalik.beerconsumer.domain.Beer;
+import com.sadatmalik.beerconsumer.exceptions.NotFoundException;
+import com.sadatmalik.beerconsumer.repositories.BeerRepository;
+import com.sadatmalik.beerconsumer.web.mappers.BeerMapper;
+import com.sadatmalik.beerconsumer.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class BeerServiceImpl implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getById(UUID beerId) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
+        );
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle().name());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUpc(beerDto.getUpc());
+
+        return beerMapper.beerToBeerDto(beerRepository.save(beer));
+    }
+}
