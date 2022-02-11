50296e887a06d51b6a91a230a372cb6d6eb86355
sadat.malik <sadat.malik@me.com>
2022-02-11 11:55:54 +0000

    Adding validate order listener

---
+++ src/main/java/com/sadatmalik/beerconsumer/services/order/BeerOrderValidationListener.java
@@ -0,0 +1,28 @@
+package com.sadatmalik.beerconsumer.services.order;
+
+import com.sadatmalik.beerconsumer.config.JmsConfig;
+import com.sadatmalik.brewery.model.events.ValidateOrderRequest;
+import com.sadatmalik.brewery.model.events.ValidateOrderResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BeerOrderValidationListener {
+
+    private final BeerOrderValidator validator;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
+    public void listen(ValidateOrderRequest validateOrderRequest){
+        Boolean isValid = validator.validateOrder(validateOrderRequest.getBeerOrder());
+
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
+                ValidateOrderResult.builder()
+                        .isValid(isValid)
+                        .orderId(validateOrderRequest.getBeerOrder().getId())
+                        .build());
+    }
+}
